ffe7b9e0d2315f8db723454f291b04a84f614a86
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-07-14 20:29:53 +0500

    02-Act01C

---
+++ Day02-Operators/Act-01/t01-02-03-04.js
@@ -0,0 +1,27 @@
+//* Write a program to add two numbers and log the result to the console.
+
+const num1 = 10;
+const num2 = 20;
+const sum = num1 + num2;
+console.log(add);
+
+//* Write a program to subtract two numbers and log the result to the console.
+
+const numb1 = 10;
+const numb2 = 20;
+const sub = numb1 - numb2;
+console.log(sub);
+
+//* Write a program to multiply two numbers and log the result to the console.
+
+const numbe1 = 10;
+const numbe2 = 20;
+const mult = numbe1 * numbe2;
+console.log(mult);
+
+//* Write a program to divide two numbers and log the result to the console.
+
+const number1 = 10;
+const number2 = 20;
+const div = number1 / number2;
+console.log(div);
---
+++ Day02-Operators/Act-01/t05.js
@@ -0,0 +1,6 @@
+//* Write a program to find the remainder when one number is divided by another and log the result to the console.
+
+const num1 = 10;
+const num2 = 2;
+const remainder = num1 % num2;
+console.log(remainder);
