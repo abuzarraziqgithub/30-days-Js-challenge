853c091952b0a17799e9cc92f733ba857c88a94d
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-07-14 22:26:04 +0500

    Day-02C

---
+++ Day02-Operators/Act-05/t14.js
@@ -0,0 +1,4 @@
+//* Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+const num1 = 10;
+const ternaryOp = num1 > 0 ? "Positive Number" : "Negative Number";
+console.log(ternaryOp);
