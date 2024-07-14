815c0fd1bdf30d07b8b2f4a77c0ea740879fcc9f
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-07-14 22:21:51 +0500

    Act-004C

---
+++ Day02-Operators/Act-04/t11.js
@@ -0,0 +1,14 @@
+//* Write a program that uses the && operator to combine two conditions and log the result to the console.
+const a = 10;
+const b = 10;
+const condition = a === b && a > b;
+console.log(condition);
+
+//* Write a program that uses the || operator to combine two conditions and log the result to the console.
+const x = 10;
+const y = 10;
+console.log(x === y || x > y);
+
+//* Write a program that uses ! operator to negate a condition and log the result to the console.
+console.log(10 !== 20);
+console.log(10 !== 10);
