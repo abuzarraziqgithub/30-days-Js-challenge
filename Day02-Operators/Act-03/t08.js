bead44c4a2203bc027f1096b951d13c09e27f1fd
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-07-14 22:16:00 +0500

    act-03C

---
+++ Day02-Operators/Act-03/t08.js
@@ -0,0 +1,17 @@
+//* Write a program to compare two numbers using > and < and log the result to the console.
+const a = 10;
+const b = 20;
+console.log(a > b);
+console.log(a < b);
+
+//* Write a program to compare two numbers using >= and <= and log the result to the console.
+const x = 1;
+const y = 2;
+console.log(x >= y);
+console.log(x <= y);
+
+//* Write a program to compare two numbers using == and === and log the result to the console.
+const z = 10;
+const k = 10;
+console.log(z == k);
+console.log(z === k);
---
+++ Day02-Operators/Act02/t06.js
@@ -0,0 +1,7 @@
+//* Use the += operator to add a number to a variable and log the result to the console.
+const num1 = 20;
+console.log((num1 += 12));
+
+//* Use the -= operator to subtract a number to a variable and log the result to the console.
+const num2 = 20;
+console.log((num2 -= 10));
