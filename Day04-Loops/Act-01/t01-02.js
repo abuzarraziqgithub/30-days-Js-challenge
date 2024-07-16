1d2bdf95771ce6090ef5576c48472f222921f536
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-07-16 20:37:34 +0500

    task-01-C

---
+++ Day04-Loops/Act-01/t01-02.js
@@ -0,0 +1,11 @@
+//* Write a program to print numbers from 1 to 10 using a for loop.
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+//* Write a program to print the multiplication table of 5 using for loop.
+let mult = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${mult} * ${i} = ${mult * i}`);
+}
