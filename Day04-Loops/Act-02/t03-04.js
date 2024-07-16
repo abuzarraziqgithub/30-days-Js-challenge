73a86681f4f47af68a2c5f3df26a0ee837bb98ae
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-07-16 21:03:13 +0500

    Act-02C

---
+++ Day04-Loops/Act-02/t03-04.js
@@ -0,0 +1,18 @@
+//* Write a program to calculate the sum of numbers from 1 to 10 using while loop.
+let i = 1;
+let sum = 0;
+
+while (i <= 10) {
+  sum += i;
+  i++;
+}
+console.log("The sum of numbers from 1 to 10 is:", sum);
+
+//* Write a program to print numbers from 10 to 1 using while loop.
+
+let y = 10;
+
+while (y >= 1) {
+  console.log(y);
+  y--;
+}
